e78b7d0626bfd43c9ad77a0ab77b1e421b11fb58
urkourbieta <Github3212>
2019-10-28 13:46:06 +0100

    bf

---
+++ ProgramacionMultimedia/FragmentsActivity/app/src/main/java/com/example/fragmentsactivity/Fragment2.java
@@ -0,0 +1,21 @@
+package com.example.fragmentsactivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        LinearLayout layout=(LinearLayout) inflater.inflate(R.layout.layout_fragment2,container);
+
+        return layout;
+    }
+}
