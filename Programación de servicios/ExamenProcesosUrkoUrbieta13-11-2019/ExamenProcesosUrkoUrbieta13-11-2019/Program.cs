27716884e0312c567d089393948877a70389e7c8
urkourbieta <Github3212>
2019-11-14 08:02:03 +0100

    bm

---
+++ Programación de servicios/ExamenProcesosUrkoUrbieta13-11-2019/ExamenProcesosUrkoUrbieta13-11-2019/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+
+namespace ExamenProcesosUrkoUrbieta13_11_2019
+{
+    class Program
+    {
+        public static int[] nums;
+        public static bool inicializado = false;
+        static void Main(string[] args)
+        {
+            NamedPipeServerStream npss;
+            StreamWriter sw;
+            StreamReader sr;
+            int opc;
+              
+            npss = new NamedPipeServerStream("server", PipeDirection.InOut);
+            npss.WaitForConnection();
+
+            sw = new StreamWriter(npss);
+            sr = new StreamReader(npss);
+            sw.AutoFlush = true;
+
+            enviarMenu(sw);
+            opc=int.Parse(sr.ReadLine());
+            enviarOpcionMenu(opc, sw, sr);
+            while (opc != 4)
+            {
+                enviarMenu(sw);
+                opc = int.Parse(sr.ReadLine());
+                enviarOpcionMenu(opc, sw, sr);
+            }
+            npss.Close();
+            //Console.ReadLine();
+        }
+
+        static void enviarMenu(StreamWriter sw) {            
+            sw.WriteLine("1.Cargar tabla");
+            sw.WriteLine("2.Ver pares");
+            sw.WriteLine("3.Ver impares");
+            sw.WriteLine("4.Salir");
+        }
+
+        static void enviarOpcionMenu(int opc, StreamWriter sw, StreamReader sr) {
+            int max;            
+            switch (opc)
+            {
+                case 1:                    
+                    sw.WriteLine("cargando");
+                    max=int.Parse(sr.ReadLine());
+                    nums = new int[max];
+                    for(int i = 0;i< max;i++)
+                        nums[i] = int.Parse(sr.ReadLine());
+                    inicializado = true;
+                    break;
+                    
+                case 2:
+                    if (!inicializado)
+                    {
+                        error(sw);
+                        break;
+                    }
+                    sw.WriteLine("pares");
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (nums[i] % 2 == 0)
+                            sw.WriteLine(nums[i]);
+                    }
+                    sw.WriteLine(-1);
+                    break;
+                case 3:
+                    if (!inicializado)
+                    {
+                        error(sw);
+                        break;
+                    }
+                    sw.WriteLine("impares");
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        if (nums[i] % 2 == 1)
+                            sw.WriteLine(nums[i]);
+                    }
+                    sw.WriteLine(-1);
+                    break;
+                case 4:
+                    sw.WriteLine("salir");
+                    break;
+            }
+                
+        }
+
+        static void error(StreamWriter sw) {
+            sw.WriteLine("error");
+        }
+    }
+}
---
+++ Programación de servicios/ExamenProcesosUrkoUrbieta13-11-2019/hijo/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace hijo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedPipeClientStream npcs;
+            StreamWriter sw;
+            StreamReader sr;
+            String opcion;
+            
+            
+            npcs = new NamedPipeClientStream(".", "server", PipeDirection.InOut);
+            npcs.Connect();
+            sw = new StreamWriter(npcs);
+            sr = new StreamReader(npcs);            
+            sw.AutoFlush = true;
+
+            leerMenu(sr, sw);
+            opcion = sr.ReadLine();
+            hacerOpcion(opcion, sw, sr);
+            while (opcion != "salir")
+            {
+                leerMenu(sr, sw);
+                opcion = sr.ReadLine();
+                hacerOpcion(opcion, sw, sr);
+            }
+            npcs.WaitForPipeDrain();
+            npcs.Close();
+        }
+        static void leerMenu(StreamReader sr, StreamWriter sw) {
+            String[] menu = new String[4];
+            String o;
+            int opc=0;
+            menu[0] = sr.ReadLine();
+            menu[1] = sr.ReadLine();
+            menu[2] = sr.ReadLine();
+            menu[3] = sr.ReadLine();
+
+            Console.Clear();
+
+            do
+            {
+                
+                for (int i = 0; i < 4; i++)
+                    Console.WriteLine(menu[i]);
+                Console.WriteLine("Escribe un número");
+                o=Console.ReadLine();
+                while (!int.TryParse(o,out opc)) {
+                    Console.WriteLine("Escribe un número");
+                    o = Console.ReadLine();
+                }
+                Console.Clear();
+            } while(opc<0||opc>4);
+
+            sw.WriteLine(opc);
+        }
+
+        static void hacerOpcion(String opcion, StreamWriter sw, StreamReader sr)
+        {
+            int max, num;
+            if (opcion == "cargando")
+            {
+                Console.WriteLine("cargando");
+                Console.WriteLine("Escribe un límite de números");
+                max = int.Parse(Console.ReadLine());
+                sw.WriteLine(max);
+                for (int i = 0; i < max; i++)
+                {
+                    num = int.Parse(Console.ReadLine());
+                    sw.WriteLine(num);
+                }
+                Console.WriteLine("números enviados, pulse una tecla para continuar");
+                Console.ReadLine();
+            }
+            else if (opcion == "pares")
+            {
+                Console.WriteLine("pares");
+                num = int.Parse(sr.ReadLine());
+                while (num != -1)
+                {
+                    Console.WriteLine(num);
+                    num = int.Parse(sr.ReadLine());
+                }
+                Console.WriteLine("números pares mostrados, pulse una tecla para continuar");
+                Console.ReadLine();
+            }
+            else if (opcion == "impares")
+            {
+                Console.WriteLine("impares");
+                num = int.Parse(sr.ReadLine());
+                while (num != -1)
+                {
+                    Console.WriteLine(num);
+                    num = int.Parse(sr.ReadLine());
+                }
+                Console.WriteLine("números impares mostrados, pulse una tecla para continuar");
+                Console.ReadLine();
+            }
+            else if (opcion == "error")
+            {
+                Console.WriteLine("Error, carga el array primero");
+                Console.ReadLine();
+            }
+            else if (opcion == "salir") {
+                Console.WriteLine("Saliendo del programa, pulse una tecla para continuar");
+                Console.ReadLine();
+            }
+        }
+    }
+}
