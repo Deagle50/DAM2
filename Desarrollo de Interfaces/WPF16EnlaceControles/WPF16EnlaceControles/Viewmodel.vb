d8300380b9448d795b19fe711836f48a76570fb6
urkourbieta <Github3212>
2020-02-10 11:58:15 +0100

    ez

---
+++ Desarrollo de Interfaces/WPF16EnlaceControles/WPF16EnlaceControles/Viewmodel.vb
@@ -0,0 +1,29 @@
+﻿Public Class Viewmodel
+    Dim Empleado As WPF16EnlaceControles.Employees
+    Private modelo As New WPF16EnlaceControles.NorthwindEntities
+    Public Property Name
+        Get
+            If Not Empleado Is Nothing Then
+                Return Empleado.FirstName
+            End If
+            Return Empleado.FirstName
+        End Get
+        Set
+    End Property
+    Public Property Surname
+        Get
+            If
+        End Get
+        Set(value)
+
+        End Set
+    End Property
+    Public Property ID
+        Get
+
+        End Get
+        Set(value)
+
+        End Set
+    End Property
+End Class
