6e06f685bb046a0d3015544ee493626ffe752a7b
urkourbieta <Github3212>
2020-02-11 12:29:45 +0100

    fc

--- Desarrollo de Interfaces/WPF16EnlaceControles/WPF16EnlaceControles/Viewmodel.vb
+++ Desarrollo de Interfaces/WPF16EnlaceControles/WPF16EnlaceControles/Viewmodel.vb
@@ -1,29 +1,49 @@
 ﻿Public Class Viewmodel
     Dim Empleado As WPF16EnlaceControles.Employees
     Private modelo As New WPF16EnlaceControles.NorthwindEntities
-    Public Property Name
+    Public Property Nombre
         Get
-            If Not Empleado Is Nothing Then
-                Return Empleado.FirstName
+            If Empleado Is Nothing Then
+                Return ""
             End If
             Return Empleado.FirstName
         End Get
-        Set
-    End Property
-    Public Property Surname
-        Get
-            If
-        End Get
         Set(value)
-
+            If Not Empleado Is Nothing Then
+                Empleado.FirstName = value
+            End If
         End Set
     End Property
-    Public Property ID
-        Get
 
+    Public Property Apellido
+        Get
+            If Empleado Is Nothing Then
+                Return ""
+            End If
+            Return Empleado.LastName
         End Get
         Set(value)
-
+            If Not Empleado Is Nothing Then
+                Empleado.LastName = value
+            End If
         End Set
     End Property
+
+    Public Property Id As Integer
+        Get
+            Return _Id
+        End Get
+        Set(value As Integer)
+            _Id = value
+        End Set
+    End Property
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+    Public Sub Buscar()
+        Empleado = modelo.Employees.Where(Function(x) x.EmployeeID = Id).FirstOrDefault
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Nombre"))
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs("Apellido"))
+
+    End Sub
 End Class
